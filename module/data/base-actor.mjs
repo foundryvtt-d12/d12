6776df3527eca0a4973102a844ffed21400ad9eb
Flavien Charlon <flavien@charlon.org>
2025-10-14 22:26:51 +0100

    Fix an issue in the D12ActorBase class

--- module/data/base-actor.mjs
+++ module/data/base-actor.mjs
@@ -8,12 +8,12 @@ export default class D12ActorBase extends D12DataModel {
     const schema = {};
 
     schema.health = new fields.SchemaField({
-      value: new fields.NumberField({ ...requiredInteger, initial: 10, min: 0 }),
-      max: new fields.NumberField({ ...requiredInteger, initial: 10 })
-    });
-    schema.power = new fields.SchemaField({
       value: new fields.NumberField({ ...requiredInteger, initial: 5, min: 0 }),
-      max: new fields.NumberField({ ...requiredInteger, initial: 5 })
+      max: new fields.NumberField({ ...requiredInteger, initial: 5, min: 1 })
+    });
+    schema.mana = new fields.SchemaField({
+      value: new fields.NumberField({ ...requiredInteger, initial: 3, min: 0 }),
+      max: new fields.NumberField({ ...requiredInteger, initial: 3, min: 0 })
     });
     schema.biography = new fields.StringField({ required: true, blank: true }); // equivalent to passing ({initial: ""}) for StringFields
 
