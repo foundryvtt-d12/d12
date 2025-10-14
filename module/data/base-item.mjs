6ef24009db7d67899134f014d93ec36430bc0e72
Flavien Charlon <flavien@charlon.org>
2025-10-14 22:26:29 +0100

    Add an ability field to items, spells and feats

--- module/data/base-item.mjs
+++ module/data/base-item.mjs
@@ -7,6 +7,19 @@ export default class D12ItemBase extends D12DataModel {
     const schema = {};
 
     schema.description = new fields.StringField({ required: true, blank: true });
+    schema.ability = new fields.StringField({
+      required: true,
+      blank: true,
+      initial: "",
+      choices: {
+        "": "None",
+        "str": "D12.Ability.Strength.long",
+        "agi": "D12.Ability.Agility.long",
+        "int": "D12.Ability.Intelligence.long",
+        "per": "D12.Ability.Perception.long",
+        "cha": "D12.Ability.Charisma.long"
+      }
+    });
 
     return schema;
   }
